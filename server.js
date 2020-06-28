2035da98a3084e9982e9e7aaa9b58ba755bec973
javier <javier.san.mail@gmail.com>
2020-06-28 19:03:30 +0200

    reestructure server.js

--- server.js
+++ server.js
@@ -1,44 +1,49 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-const moment = require('moment');
+const path = require("path");
+const mqtt = require("mqtt");
+const mosca = require("mosca");
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const moment = require("moment");
 
-server.listen(80);
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+//------------NODE SERVER-------------------
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-
+//
+io.on("connection", function (socket) {
+  socket.emit("news", { hello: "world" });
 });
 
-/////////
-const mqtt = require('mqtt');
-var mosca = require('mosca');
+//-------------MQTT SERVER-------------------
 var settings = {
-    port: 1883
+  port: 1883,
 };
 var server = new mosca.Server(settings);
-server.on('ready', function setup() {
-    console.log('Mosca server is up and running');
-});
-server.on('clientConnected', function (client) {
-    console.log('client connected', client.id);
+
+server.on("ready", function setup() {
+  console.log("Mosca server is up and running");
 });
 
-var client = mqtt.connect('mqtt://localhost');
-client.subscribe('temperature');
-client.on('message', function (topic, message) {
+server.on("clientConnected", function (client) {
+  console.log("client connected", client.id);
+});
 
-    const data = {
-        sensorData: JSON.parse(message),
-        //temperature: message.toString()   
-        time: moment().format('HH:mm:ss')
-    };
-    console.log(data)
-    io.emit('temperature', data);
-})
+//-----------------------MQTT CLIENT----------------------
+var client = mqtt.connect("mqtt://localhost");
 
+client.subscribe("temperature");
 
-
+client.on("message", function (topic, message) {
+  const data = {
+    sensorData: JSON.parse(message),
+    //temperature: message.toString()
+    time: moment().format("HH:mm:ss"),
+  };
+  console.log(data);
+  io.emit("temperature", data);
+});
